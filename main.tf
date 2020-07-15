95076a9b4c89d11dbd260ce20993d69d69d83110
skhedim <46813029+skhedim@users.noreply.github.com>
2020-07-15 12:09:27 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -88,6 +88,20 @@ resource "aws_key_pair" "deployer" {
   public_key = tls_private_key.example.public_key_openssh
 }
 
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public-b.id
+  key_name      = aws_key_pair.deployer.id
+  associate_public_ip_address = true
+  user_data     = file("${path.module}/postinstall.sh")
+  vpc_security_group_ids =  ["${aws_security_group.allow_http.id}"]
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -104,10 +118,38 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical 
 }
 
+resource "aws_security_group" "allow_http" {
+  name        = "allow_tls"
+  description = "Allow http inbound traffic"
+  vpc_id      = "${aws_vpc.default.id}"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  egress {
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_http-tf"
+  }
+}
+
 output "private-key" {
   value = tls_private_key.example.private_key_pem
 }
 
 output "ami-value" {
   value = data.aws_ami.ubuntu.image_id
+}
+
+output "public-ip" {
+  value = aws_instance.web.public_ip
 }
